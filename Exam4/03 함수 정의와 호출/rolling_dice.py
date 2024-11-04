dc1854a1ea0e56104d5c0fac0cf2a15e094770bf
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-04 21:26:09 +0900

    rolling_dice를 활용한 정의된 함수 호출 코드

---
+++ Exam4/03 함수 정의와 호출/rolling_dice.py
@@ -0,0 +1,10 @@
+import random
+
+def rolling_dice(pip=6, repeat=1):
+    for r in range(1, repeat + 1):
+        n = random.randint(1, pip)
+        print(pip, "면 주사위 굴린 결과", r, " : ", n)
+
+rolling_dice()
+rolling_dice(2, 4)
+rolling_dice(pip=3, repeat=1)
