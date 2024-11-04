cf2c0cfd7dda9bdd94907315bd9e2c3a3a5b8e2e
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-04 20:07:32 +0900

    주사위 굴리는 프로그램 1

---
+++ Exam4/03 함수 정의와 호출/dice1.py
@@ -0,0 +1,9 @@
+import random   # 임의의 값을 얻기 위해 사용하는 random 모듈을 가져온다.
+
+# 처음 시작
+n = random.randint(1, 6)    # 1에서 6까지의 정수 중에 하나를 뽑는다.
+print("결과: ", n)
+n = random.randint(1, 6)
+print("결과: ", n)
+n = random.randint(1, 6)
+print("결과: ", n)
