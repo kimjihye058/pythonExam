f926bbebd5bfc95c7a845dc8031a71db8cd71666
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-04 22:26:32 +0900

    반 책상 자리 배치도 예제

---
+++ Exam4/04 리턴 값이 있는 함수/position.py
@@ -0,0 +1,16 @@
+import random
+def position(number, title, column):
+    print(title)
+    students = list(range(1, number+1))
+    random.shuffle(students)
+    pos = ""
+    for i in range(0, number):
+        if students[i] < 10:
+            pos = pos + "0" + str(students[i]) + "번\t"
+        else:
+            pos = pos + str(students[i]) + "번\t"
+        if (i+1) % column == 0:
+            pos = pos + "\n"
+    print(pos)
+
+position(20, title="3-2반", column=5)
