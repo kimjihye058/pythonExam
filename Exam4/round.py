90b913bd7fc341fa88cc1f871338b35b98fc8cb9
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-04 19:02:15 +0900

    round() 함수

---
+++ Exam4/round.py
@@ -0,0 +1,6 @@
+pi = 3.14152
+print(pi, "의 소숫점 1자리 반올림은", round(pi))    # 반올림 자릿수를 생략할 경우 소수 첫째 자리에서 반올림 함
+print(pi, "의 소숫점 1자리 반올림은", round(pi, 0))
+print(pi, "의 소숫점 2자리 반올림은", round(pi, 1))
+print(pi, "의 소숫점 3자리 반올림은", round(pi, 2))
+print(pi, "의 소숫점 4자리 반올림은", round(pi, 3))
